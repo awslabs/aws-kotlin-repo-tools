1708152e12a5e470ec927c27dfd6bc38270fcba6
Aaron J Todd <todaaron@amazon.com>
2023-07-26 11:33:58 -0400

    downgrade gradle and set target compat

--- build-plugins/build.gradle.kts
+++ build-plugins/build.gradle.kts
@@ -2,6 +2,9 @@
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
+
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 buildscript {
     repositories {
         mavenCentral()
@@ -32,4 +35,16 @@ gradlePlugin {
             implementationClass = "aws.sdk.kotlin.gradle.kmp.KmpDefaultsPlugin"
         }
     }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+        freeCompilerArgs += listOf("-Xopt-in=kotlin.RequiresOptIn")
+    }
+}
+
+tasks.withType<JavaCompile> {
+    sourceCompatibility = JavaVersion.VERSION_1_8.toString()
+    targetCompatibility = JavaVersion.VERSION_1_8.toString()
 }
