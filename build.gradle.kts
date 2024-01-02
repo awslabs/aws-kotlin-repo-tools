f873a0f18861c49bb69d816134984e0aa01fe174
Aaron Todd <aajtodd@users.noreply.github.com>
2024-01-02 13:48:44 -0500

    publish releases to private s3 maven repo (#30)

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,11 +6,16 @@ plugins {
     `maven-publish`
 }
 
+val releaseVersion = findProperty("release.version") as? String
+if (releaseVersion == null) logger.warn("no release version set")
+
+val s3Url = propertyOrEnv("release.s3.url", "RELEASE_S3_URL")
+if (s3Url == null) logger.warn("S3 repository not configured, missing S3 url")
+
 subprojects {
     // FIXME - do we want this to be the group?
     group = "aws.sdk.kotlin"
-    // FIXME - set dynamically
-    version = "0.3.2"
+    version = releaseVersion ?: "0.0.1"
 
     apply(plugin = "maven-publish")
     publishing {
@@ -19,8 +24,25 @@ subprojects {
                 name = "testLocal"
                 url = rootProject.layout.buildDirectory.dir("m2").get().asFile.toURI()
             }
+
+            if (s3Url != null) {
+                maven {
+                    name = "release"
+                    url = java.net.URI(s3Url)
+                    credentials(AwsCredentials::class.java) {
+                        accessKey = propertyOrEnv("aws.accessKeyId", "AWS_ACCESS_KEY_ID")
+                        secretKey = propertyOrEnv("aws.secretAccessKey", "AWS_SECRET_ACCESS_KEY")
+                        sessionToken = propertyOrEnv("aws.sessionToken", "AWS_SESSION_TOKEN")
+                    }
+                }
+            }
         }
     }
+}
+
+fun propertyOrEnv(propName: String, envName: String): String? {
+    val env = System.getenv()
+    return findProperty(propName) as? String ?: env[envName]
 }
 
 // chicken and egg problem, we can't use the kotlinter gradle plugin here AND use our custom rules
