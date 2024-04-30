7ef49306a346bcd1c73e9d28ed88983cffec7e1c
0marperez <operez899999@gmail.com>
2024-04-30 12:29:01 -0400

    fix: readability improvements

--- build-plugins/build-support/src/main/kotlin/aws/sdk/kotlin/gradle/plugins/artifactsizemetrics/AnalyzeArtifactSizeMetrics.kt
+++ build-plugins/build-support/src/main/kotlin/aws/sdk/kotlin/gradle/plugins/artifactsizemetrics/AnalyzeArtifactSizeMetrics.kt
@@ -16,6 +16,7 @@ import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 import java.io.File
+import kotlin.math.abs
 
 /**
  * Gradle task that analyzes/compares a project's local artifact size metrics to
@@ -59,8 +60,8 @@ internal abstract class AnalyzeArtifactSizeMetrics : DefaultTask() {
         val analysis = analyzeArtifactSizeMetrics(latestReleaseMetrics, currentMetrics)
 
         hasSignificantChangeFile.get().asFile.writeText(analysis.significantChange.toString())
-        val diffTable = createDiffTable(analysis)
-        val output = if (analysis.hasDelta) diffTable else noDiffMessage
+        val diffTables = createDiffTables(analysis)
+        val output = if (analysis.hasDelta) diffTables else noDiffMessage
 
         this.analysisFile.get().asFile.writeText(output)
     }
@@ -100,7 +101,7 @@ internal abstract class AnalyzeArtifactSizeMetrics : DefaultTask() {
             )
         }
 
-        val changeHappened = artifactSizeMetrics.values.any { it.delta != 0L }
+        val changeHappened = artifactSizeMetrics.values.any { it.delta.isNotaFluctuation() }
         val significantChange = artifactSizeMetrics.values.any {
             (it.percentage > pluginConfig.significantChangeThresholdPercentage) || // Increase in size above threshold
                 (it.latestReleaseSize == 0L) // New artifact
@@ -109,33 +110,72 @@ internal abstract class AnalyzeArtifactSizeMetrics : DefaultTask() {
         return ArtifactSizeMetricsAnalysis(artifactSizeMetrics, significantChange, changeHappened)
     }
 
+    // There are small fluctuations in artifact size that are not real delta
+    private fun Long.isNotaFluctuation() = abs(this) > 5L
+
     private data class ArtifactSizeMetricsAnalysis(
         val metrics: Map<String, ArtifactSizeMetric>,
         val significantChange: Boolean,
         val hasDelta: Boolean,
     )
 
-    private fun createDiffTable(analysis: ArtifactSizeMetricsAnalysis): String = buildString {
+    private fun createDiffTables(analysis: ArtifactSizeMetricsAnalysis): String = buildString {
         appendLine("Affected Artifacts\n=")
-        appendLine("| Artifact |Pull Request (bytes) | Latest Release (bytes) | Delta (bytes) | Delta (percentage) |")
-        appendLine("| -------- | ------------------: | ---------------------: | ------------: | -----------------: |")
-        analysis.metrics.forEach { metric ->
-            if (metric.value.delta != 0L) {
-                append("|")
-                append(metric.key)
-                append("|")
-                if (metric.value.currentSize == 0L) append("(does not exist)") else append("%,d".format(metric.value.currentSize))
-                append("|")
-                if (metric.value.latestReleaseSize == 0L) append("(does not exist)") else append("%,d".format(metric.value.latestReleaseSize))
-                append("|")
-                append("%,d".format(metric.value.delta))
-                append("|")
-                append("%.2f".format(metric.value.percentage))
-                append("%")
-                appendLine("|")
+
+        val requiresAttention = StringBuilder()
+            .appendLine("Significantly increased in size")
+            .appendLine()
+            .append(tableHeader)
+        var artifactRequiresAttention = false
+
+        val ordinary = StringBuilder()
+            .appendLine("<details>") // See: https://tiny.amazon.com/zhuf9fhk/docsgithengetswritworkorga
+            .appendLine("<summary>Changed in size</summary>")
+            .appendLine()
+            .append(tableHeader)
+        var artifactOrdinaryChange = false
+
+        analysis.metrics
+            .toList()
+            .sortedBy { it.second.percentage }.toMap()
+            .forEach { metric ->
+                if (metric.value.delta.isNotaFluctuation()) {
+                    val row = buildString {
+                        append("|")
+                        append(metric.key)
+                        append("|")
+                        if (metric.value.currentSize == 0L) append("(does not exist)") else append("%,d".format(metric.value.currentSize))
+                        append("|")
+                        if (metric.value.latestReleaseSize == 0L) append("(does not exist)") else append("%,d".format(metric.value.latestReleaseSize))
+                        append("|")
+                        append("%,d".format(metric.value.delta))
+                        append("|")
+                        append("%.2f".format(metric.value.percentage))
+                        append("%")
+                        if (metric.value.requiresAttention()) append("⚠️")
+                        appendLine("|")
+                    }
+
+                    if (metric.value.requiresAttention()) {
+                        requiresAttention.append(row)
+                        artifactRequiresAttention = true
+                    } else {
+                        ordinary.append(row)
+                        artifactOrdinaryChange = true
+                    }
+                }
             }
-        }
+
+        ordinary
+            .appendLine()
+            .append("</details>")
+
+        if (artifactRequiresAttention) appendLine(requiresAttention)
+        if (artifactOrdinaryChange) appendLine(ordinary)
     }
+
+    private fun ArtifactSizeMetric.requiresAttention() =
+        this.percentage > pluginConfig.significantChangeThresholdPercentage || this.percentage.isNaN()
 
     private data class ArtifactSizeMetric(
         val currentSize: Long,
@@ -162,4 +202,9 @@ internal abstract class AnalyzeArtifactSizeMetrics : DefaultTask() {
         =
         No artifacts changed size
     """.trimIndent()
+
+    private val tableHeader = buildString {
+        appendLine("| Artifact |Pull Request (bytes) | Latest Release (bytes) | Delta (bytes) | Delta (percentage) |")
+        appendLine("| -------- | ------------------: | ---------------------: | ------------: | -----------------: |")
+    }
 }
