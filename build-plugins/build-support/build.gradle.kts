91734443b5fca955518592508ea98164a8d495a4
Ian Botsford <83236726+ianbotsf@users.noreply.github.com>
2025-07-01 16:41:48 +0000

    chore: pin SDK client versions to avoid non-deterministic build failures

--- build-plugins/build-support/build.gradle.kts
+++ build-plugins/build-support/build.gradle.kts
@@ -26,8 +26,8 @@ dependencies {
 
     implementation(libs.nexusPublishPlugin)
     compileOnly(gradleApi())
-    implementation("aws.sdk.kotlin:s3:1.4.+")
-    implementation("aws.sdk.kotlin:cloudwatch:1.4.+")
+    implementation(libs.aws.sdk.s3)
+    implementation(libs.aws.sdk.cloudwatch)
     testImplementation(libs.junit.jupiter)
 }
 
